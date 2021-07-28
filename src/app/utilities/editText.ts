a6103458db543d9cdff1291e9752e276a642c7cb
JohnMoran <johnnymoran44@hotmail.com>
2021-07-28 17:16:12 +0200

    now working with verbs

--- src/app/utilities/editText.ts
+++ src/app/utilities/editText.ts
@@ -3,8 +3,9 @@ import { objectE } from "../dictionaries/objects/objectE";
 import { objectI } from "../dictionaries/objects/objectI";
 import { objectO } from "../dictionaries/objects/objectO";
 import { objectU } from "../dictionaries/objects/objectU";
+import { objVerbsCleaned } from "../dictionaries/objects/objVerbsCleaned";
 
-//á, é, í, ó, ú
+//á, é, í, ó, ú, ñ
 /* let Os = a.filter((item) => item.includes("ú"));
 		console.log(Os); */
 
@@ -12,12 +13,25 @@ import { objectU } from "../dictionaries/objects/objectU";
 		UsSin.forEach((key, i) => (keyValues[key] = Us[i]));
 		console.log(keyValues); */
 
+/* let keyValues: any = {};
+		verbsSin.forEach((key, i) => (keyValues[key] = verbs[i]));
+		console.log(keyValues);
+	
+		let cleanObject: any = {};
+		for (const [key, value] of Object.entries(objectVerbs)) {
+			if (value.includes("á") || value.includes("é") || value.includes("í") || value.includes("ó") || value.includes("ú") || value.includes("ñ")) {
+				cleanObject[key] = value;
+			}
+		}
+		*/
+
 const allObjects = {
 	...objectA,
 	...objectE,
 	...objectI,
 	...objectO,
 	...objectU,
+	...objVerbsCleaned,
 };
 
 export function editText(bodyText: string) {
@@ -29,8 +43,6 @@ export function editText(bodyText: string) {
 			}
 		}
 	});
-
 	console.log(bodyTextArray.join(" "));
-
 	return bodyTextArray.join(" ");
 }
